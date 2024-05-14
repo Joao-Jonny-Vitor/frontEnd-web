5ffd3cf6e623a7c3fcdb774221915bdeacc87ee2
João_Jonny_Vitor <joao.jonny.vitor@gmail.com>
2024-05-13 22:19:12 -0300

    aula13_05

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -1,4 +1,5 @@
-
+//adicionar o token do finnhub stock APIs
+const token = 'cp1bhl1r01qu1k1i3j4gcp1bhl1r01qu1k1i3j50'
 
 const allStocks =[{
     bolsa: "NASDAQ",
@@ -84,23 +85,42 @@ function addTable(stock){
 }
 
 function loadTable(){
-    allStocks.map(stock => addTable(stock))
+	const table = document.querySelector('#table-acoes')
+	let rows = table.innerHTML
+	allStocks.map(stock => {
+		rows += 
+			`<tr>
+				<td>${stock.bolsa}</td>
+				<td>${stock.codigo}</td>
+				<td>${stock.empresa}</td>
+				<td>${realFormat(stock.valor / 100)}</td>
+				<td>${stock.variacao} %</td>
+				<td>${stock.nAcoes}</td>
+				<td>${realFormat((stock.valor / 100) * stock.nAcoes)}</td>
+			</tr>`
+	})
+	table.innerHTML = rows
 }
 
-const openModal = () => {
-    const modal = document.getElementById("add-card-modal")
-    modal.style.display = "flex"
+const openModal = (idModal) => {
+	const modal = document.getElementById(idModal)
+	modal.style.display = 'flex'
 }
 
 const closeModal = (event, id) => {
+	if(id){
+		const modal = document.getElementById(id)
+		modal.style.display = 'none'
+		return
+	}
 
-    const modal = document.getElementById("add-card-modal")
-
-    if(event?.target?.id === 'add-card-modal' || id === 'add-card-modal'){
-        modal.style.display = "none"
-    }
-    
+	if(event?.target?.className === "modal"){
+		const modal = document.getElementById(event.target.id)
+		modal.style.display = 'none'
+		return
+	}
 }
+
 
 const createCard = (event) =>{
         event.preventDefault()
@@ -121,5 +141,42 @@ const createCard = (event) =>{
 
         event.target.reset()
 
-        closeModal(null, 'add-card-modal')
+        closeModal(null, 'add-form-modal')
+}
+
+// async function testeAPI(){
+
+// }
+
+const testeApi = async () => {
+	const response = await fetch('https://finnhub.io/api/v1/quote?symbol=AAPL&token=cp1aj71r01qu1k1i369gcp1aj71r01qu1k1i36a0')
+	//const response = await fetch('https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=cp1aj71r01qu1k1i369gcp1aj71r01qu1k1i36a0')
+	
+	console.log(response)
+	const result = await response.json()
+	console.log(result.objectName)
+}
+
+const createApiCard = async (event) =>{
+	event.preventDefault()
+	const {codigo, nAcoes} = event.target.elements
+
+	const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${codigo.value}&token=${token}`)
+	const result = await response.json()
+
+	const response2 = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${codigo.value}&token=${token}`)
+	const profile = await response2.json()
+
+	const stock = {
+		bolsa: profile.exchange.split(' ')[0],
+		codigo: codigo.value,
+		empresa: profile.name,
+		valor: result.c * 100,
+		variacao: result.d,
+		nAcoes: nAcoes.value
+	}
+
+	addCard(stock)
+	event.target.reset()
+	closeModal(null, 'add-api-modal')
 }
